6ee1aff5754127932626127b24d13046e0df9754
max-sliva <persimon@inbox.ru>
2024-03-25 19:56:05 +0500

    added scrollable option for text

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
@@ -27,36 +28,37 @@ import java.io.*
 @Composable
 fun App() {
     val curPath = System.getProperty("user.dir")
+    val backgroundImage = "items/background.jpg"
     println("user dir = $curPath")
-    var filesSet = remember { mutableStateListOf("$curPath/welcome.txt", "$curPath/НВГУ_лого с корпусом.png") }
+    var filesSet = remember { mutableStateListOf("$curPath/welcome.txt", "$curPath/$backgroundImage") }
     val itemsDir = "$curPath/items/copy_Rarit"
     val dirsList = listDirsUsingDirectoryStream(itemsDir)
     println("dirsList = $dirsList")
     //var setOfItems =
 //    var itemsMap = mutableMapOf<String, String>()
-    var itemsMap2 = mutableMapOf<String, Set<String>>()
+    var itemsMap2 = mutableMapOf<String, Set<String>>() //мап для хранения названия экспоната и набора из его описания и картинки
     dirsList.forEach {
         val filesList = listFilesUsingDirectoryStream("$itemsDir/$it")
         val txtFile = filesList.find { it.contains(".txt") }
-        val imgFile = filesList.find { it!=txtFile }
+        val imgFile = filesList.find { it != txtFile }
         val txtFilePath = "$itemsDir/$it/$txtFile"
         val imgFilePath = "$itemsDir/$it/$imgFile"
 //        val filesSet = setOf(txtFilePath, imgFilePath)
         val file = File(txtFilePath)
         val ioStream = BufferedReader(FileReader(file))
-        ioStream.readLine()
-        val s = ioStream.readLine()
+        val firstStringInFile = ioStream.readLine()
+        val s = if (firstStringInFile=="") ioStream.readLine() else firstStringInFile //если первая строка пустая
 //        println("for $it: $filesList caption = $s")
 //        itemsMap[s] = "$itemsDir/$it"
         itemsMap2[s] = setOf(txtFilePath, imgFilePath)
     }
-    itemsMap2.forEach{
+    itemsMap2.forEach {
         println("${it.key} : ${it.value}")
     }
     val fitimLogo = "fitim.png"
-    val fitimLogoWhite = "fitim_white.png"
+//    val fitimLogoWhite = "fitim_white.png"
+    val facultyLogoWhite = "faculty_white.png"
     val nvsuLogoWhite = "NVSU_white.png"
-    val backgroundImage = "items/background.jpg"
 
     MaterialTheme {
 //        Button(onClick = {
@@ -68,8 +70,8 @@ fun App() {
 //            topBar = {
 //                TopAppBar(title = {
         Column(Modifier.fillMaxSize()) {
-            UpperBar(nvsuLogoWhite, fitimLogoWhite)
-            DropdownDemo(itemsMap2.keys.toList()){
+            UpperBar(nvsuLogoWhite, facultyLogoWhite)
+            DropdownDemo(itemsMap2.keys.toList()) {
                 println("selected = $it")
                 val tempList = itemsMap2[it]?.toList()
                 filesSet.clear()
@@ -88,7 +90,7 @@ fun App() {
 }
 
 @Composable
-private fun UpperBar(nvsuLogoWhite: String, fitimLogoWhite: String) {
+private fun UpperBar(nvsuLogoWhite: String, facultyLogoWhite: String) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceEvenly,
@@ -98,14 +100,26 @@ private fun UpperBar(nvsuLogoWhite: String, fitimLogoWhite: String) {
 //                        .padding(20.dp)
             .background(Color(0xff1e63b2))
     ) {
+        val nvsuFile = File(nvsuLogoWhite)
+        val nvsuBitmap: ImageBitmap = remember(nvsuFile) {
+            loadImageBitmap(nvsuFile.inputStream())
+        }
+        val facultyFile = File(facultyLogoWhite)
+        val facultyBitmap: ImageBitmap = remember(facultyFile) {
+            loadImageBitmap(facultyFile.inputStream())
+        }
         Image(
-            painter = painterResource(nvsuLogoWhite), //указываем источник изображения
+            painter = BitmapPainter(image = nvsuBitmap),
+//            painter = painterResource(nvsuLogoWhite), //указываем источник изображения
             contentDescription = "", //можно вставить описание изображения
             contentScale = ContentScale.Fit, //параметры масштабирования изображения
         )
-        Text("НВГУ ФИТМ | ИТ Музей", color = Color.White, fontSize = 30.sp, fontWeight = FontWeight.Bold)
+        var text = File("captionText.txt").readText()
+        Text(text, color = Color.White, fontSize = 30.sp, fontWeight = FontWeight.Bold)
+//        Text("НВГУ ФИТМ | ИТ Музей", color = Color.White, fontSize = 30.sp, fontWeight = FontWeight.Bold)
         Image(
-            painter = painterResource(fitimLogoWhite), //указываем источник изображения
+            painter = BitmapPainter(image = facultyBitmap), //указываем источник изображения
+//            painter = painterResource(facultyLogoWhite), //указываем источник изображения
             contentDescription = "", //можно вставить описание изображения
             contentScale = ContentScale.Fit, //параметры масштабирования изображения
         )
@@ -124,7 +138,7 @@ fun MyContent(filesSet: SnapshotStateList<String>) {
         !it.contains(".txt")
     }
 //    var text = File("$curPath/items/copy_Rarit/alfa/Кинокамера_Киев-16_Альфа_Полуавтомат.txt").readText().replace("\t","   ")
-    var text = File("$textFile").readText().replace("\t","   ")
+    var text = File("$textFile").readText().replace("\t", "   ")
 //    println(text)
 //    val file = File("$curPath/items/copy_Rarit/alfa/Внешний_вид_кинокамеры.jpg")
     val file = File("$imageFile")
@@ -136,14 +150,37 @@ fun MyContent(filesSet: SnapshotStateList<String>) {
         modifier = Modifier
             .fillMaxSize()
             .border(BorderStroke(2.dp, Color.Blue))
-            .padding(20.dp)
+            .padding(30.dp)
     ) {
-        Text(
-            text, fontSize = 20.sp,
-            modifier = Modifier.weight(3f)
+        val stateVertical = rememberScrollState(0)
+//        Box(
+//            modifier = Modifier
+//                .weight(5f)
+//                .verticalScroll(stateVertical)
+//        ) {
+            Text(
+                text, fontSize = 20.sp,
+                modifier = Modifier
+                    .verticalScroll(stateVertical)
+                    .weight(5f),
+                textAlign = TextAlign.Justify,
+                maxLines = 100
+
+            )
+//        }
+        VerticalScrollbar(
+            modifier = Modifier
+                .fillMaxHeight(),
+            adapter = rememberScrollbarAdapter(stateVertical)
         )
-        Image( //todo сделать увеличение картинки на весь экран по щелчку
-            modifier = Modifier.weight(1f),
+        Image(
+            modifier = Modifier
+                .weight(2f)
+                .clickable(
+                    onClick = {//todo сделать увеличение картинки на весь экран по щелчку
+                        println("image clicked")
+                    }
+                ),
             painter = BitmapPainter(image = imageBitmap),
             contentDescription = null
         )
@@ -172,19 +209,22 @@ fun main() = application {
 }
 
 @Composable
-fun DropdownDemo(itemsInitial:  List<String>, onUpdate: (x: String) -> Unit) { //комбобокс для выбора компорта для подключения к Arduino
+fun DropdownDemo(
+    itemsInitial: List<String>,
+    onUpdate: (x: String) -> Unit
+) { //комбобокс для выбора компорта для подключения к Arduino
     var expanded by remember { mutableStateOf(false) }
 //    val items = listOf("com1", "com2", "com3")
 //    val disabledValue = "B"
     var items = remember { mutableStateListOf<String>() }
     itemsInitial.forEach {
-        if (!items.contains(it))items.add(it)
+        if (!items.contains(it)) items.add(it)
     }
     var selectedIndex by remember { mutableStateOf(-1) }
     Box(modifier = Modifier.wrapContentSize(Alignment.TopStart)) {
         Text( //заголовок комбобокса
-            if (selectedIndex<0) "Выберите экспонат: ▼" //если еще ничего не выбрано
-            else items[selectedIndex]+" ▼", //если выбрано
+            if (selectedIndex < 0) "Выберите экспонат: ▼" //если еще ничего не выбрано
+            else items[selectedIndex] + " ▼", //если выбрано
             modifier = Modifier.clickable(onClick = { //при нажатии на текст раскрываем комбобокс
 //                val tempPortList = SerialPortList.getPortNames().toList() //получаем активные порты
 //                println("SerialPortList = $tempPortList")
@@ -221,7 +261,7 @@ fun DropdownDemo(itemsInitial:  List<String>, onUpdate: (x: String) -> Unit) { /
 //                    } else {
 //                        ""
 //                    }
-                    Text(text = s )
+                    Text(text = s)
                 }
             }
         }
