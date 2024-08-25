051b83afc27f75176fcdcbb9ddb6a62e445833f3
user <persimon@inbox.ru>
2024-08-25 18:41:54 +0500

    updated settings window in compose version

--- src/main/kotlin/SettingsCompose.kt
+++ src/main/kotlin/SettingsCompose.kt
@@ -1,5 +1,12 @@
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
 
 @Composable
@@ -10,12 +17,31 @@ fun SettingsWindow(isVisible: MutableState<Boolean>, choice: MutableState<Int>) 
             choice.value = 0
         },
         visible = isVisible.value,
-        undecorated = false, //если в файле mode.txt первое число 0, то без оконных кнопок, иначе они будут, для отладки
+        undecorated = false,
         alwaysOnTop = false,
         state = WindowState(WindowPlacement.Fullscreen)
 //        state = WindowState(WindowPlacement.Fullscreen)
     ) {
-        //
+        Column(Modifier.fillMaxSize()){
+            Row(//верхний ряд с управляющими элементами
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+//                        .padding(20.dp)
+                    .background(Color(0xff1e63b2))
+            ) {
+
+            }
+            Row(//центральный ряд с содержимым
+                modifier = Modifier
+                    .fillMaxSize()
+                    .border(BorderStroke(2.dp, Color(0xff1e63b2)))
+                    .padding(30.dp)
+            ) {
+
+            }
+        }
+
     }
 //    DialogWindow(visible = isVisible.value, onCloseRequest = { isVisible.value = false }) {
 //        Text("dialog content")
