8cf84a9e89c8384cb73825a2bdcc99892c1b815a
max-sliva <persimon@inbox.ru>
2024-08-29 16:49:02 +0500

    added shape for StendBox

--- src/main/kotlin/StendDataModel.kt
+++ src/main/kotlin/StendDataModel.kt
@@ -1,14 +1,15 @@
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
@@ -23,10 +24,40 @@ fun StendBox(model: StendBoxModel){
             modifier = Modifier
                 .fillMaxHeight()
                 .fillMaxWidth()
-                .background(Color(0xffff0000))
-                .border(BorderStroke(2.dp, Color(0xffff0000)))
+               // .background(Color(0xffff0000))
+            //    .border(BorderStroke(2.dp, Color(0xffff0000)))
         ) {
             Text("stend")
+            Box(
+                modifier = Modifier
+                    .size(500.dp)
+                    .fillMaxHeight()
+//                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(10.dp))
+                    //.background(Color.Red)
+                    .border(BorderStroke(2.dp, Color.Red))
+            )
+//            val hexagon = remember {
+//                RoundedPolygon(
+//                    6,
+//                    rounding = CornerRounding(0.2f)
+//                )
+//            }
+//            val clip = remember(hexagon) {
+//                RoundedPolygonShape(polygon = hexagon)
+//            }
+//            Box(
+//                modifier = Modifier
+//                    .clip(clip)
+//                    //.background(MaterialTheme.colorScheme.secondary)
+//                    .size(200.dp)
+//            ) {
+//                Text(
+//                    "Hello Compose",
+//                    //color = MaterialTheme.colorScheme.onSecondary,
+//                    modifier = Modifier.align(Alignment.Center)
+//                )
+//            }
         }
     } else if (model.type=="comp") {
         Column(
